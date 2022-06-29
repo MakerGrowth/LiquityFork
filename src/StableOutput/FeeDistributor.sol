34bce87f7e121cca6e9539809531b55dd42ec4fe
Felipe Buiras <f.morisan@gmail.com>
2022-06-29 10:13:51 -0300

    fees: implement pull, fixes, add special addrs

--- src/StableOutput/FeeDistributor.sol
+++ src/StableOutput/FeeDistributor.sol
@@ -16,9 +16,13 @@ contract FeeDistributor is IFeeDistributor {
     address public _devWallet;
     address public _lendingPool;
 
-    constructor(address borrowerOperations, IERC20 dai) public {
+    event FeeDeposit(uint256 indexed amount);
+
+    constructor(address borrowerOperations, IERC20 dai, address devWallet, address lendingPool) public {
         _borrowerOperations = borrowerOperations;
         _dai = dai;
+        _devWallet = devWallet;
+        _lendingPool = lendingPool;
     }
 
     modifier onlyBorrowerOperations() {
@@ -26,16 +30,12 @@ contract FeeDistributor is IFeeDistributor {
         _;
     }
 
-    function updateCollectedFees() external override onlyBorrowerOperations {
-        uint256 currentFees = _dai.balanceOf(address(this));
-    }
-
     function pushFees(uint256 amount) external override onlyBorrowerOperations {
-        require(
-            _dai.transferFrom(_borrowerOperations, address(this), amount),
-            "FD: DAI transferFrom failure"
-        );
+        uint256 bal = _dai.balanceOf(address(this));
+        _dai.transferFrom(_borrowerOperations, address(this), amount);
+        require(_dai.balanceOf(address(this)) > bal, "what");
         totalCollectedFees += amount;
+        emit FeeDeposit(amount);
     }
 
     function collectedFeesFor(address who) public override view returns (uint256) {
@@ -47,7 +47,7 @@ contract FeeDistributor is IFeeDistributor {
         // CASE 2: lending pool
         else if (who == _lendingPool) {
             // 90% share
-            return totalCollectedFees * 10 / 9;
+            return totalCollectedFees * 9 / 10;
         }
         // CASE 3: anyone else
         else {
@@ -57,6 +57,12 @@ contract FeeDistributor is IFeeDistributor {
     }
 
     function pullFees(uint256 amount) external override {
-        return;
+        require(availableFeesToCollect(msg.sender) >= amount, "FD: cant pull fees");
+        withdrawnFees[msg.sender] += amount;
+        _dai.transfer(msg.sender, amount);
+    }
+
+    function availableFeesToCollect(address who) public view returns (uint256) {
+        return collectedFeesFor(who) - withdrawnFees[who];
     }
 }
--- src/StableOutput/IFeeDistributor.sol
+++ src/StableOutput/IFeeDistributor.sol
@@ -5,7 +5,6 @@ interface IFeeDistributor {
     function collectedFees() external view returns (uint256);
     function pullFees(uint256 amount) external;
     function pushFees(uint256 amount) external;
-    function updateCollectedFees() external;
 
     function collectedFeesFor(address) external view returns (uint256);
 }
